a328a430fe2dfcc962d9decfc1331246dfc7b910
Riyas <m.riyas.muhammed@gmail.com>
2025-03-25 17:03:53 +0530

    Updates

---
+++ experianceData.js
@@ -0,0 +1,30 @@
+export const ProjectsList = [
+    {
+      id: 1,
+      title: "WEBSITE WITH VITE REACT",
+      desc: " Very simply designed Cafe 4key officail Website ,used 3D objects ,animations,email and parelex effects for eye catching.It is live on www.cafe4key.com Now!",
+      img: ["cafe4key1.jpeg", "cafe4key2.jpg", "cafe4key3.jpg","cafe4key4.jpg"],
+      tech: ["react", "css", "framer-motion"],
+      demo: "https://riyasteche.github.io/cafe4key-web/",
+      code: "https://github.com/RiyasTeche/cafe4key-web/tree/main/src",
+    },
+    {
+      id: 2,
+      title: "FULL STACK APP FOR MOSQUE AUTOMATION",
+      desc: "A full stack app having cred operations for inhabitans for a mohalla,Admin page ,authentication and routers used for the work, and Mongo DB used in Back end for store the data!",
+      img: ["masjid1.jpg", "masjid2.jpeg"],
+      tech: ["react", "css", "navigation", "mongo"],
+      demo: "",
+      code: "https://github.com/RiyasTeche/masjid-soft/tree/master/masjid-client",
+    },
+    {
+      id: 3,
+      title: "PORTFOLIO WEBSITE WITH VITE",
+      desc: "My refined portfolio Website ,animations,email integration,routers for page switching and parelex effects for smooth viewing experiance!",
+      img: ["portfolio1.jpg","portfolio2.jpg","portfolio3.jpg","portfolio4.jpg","portfolio5.jpg","portfolio6.jpg"],
+      tech: ["react", "css", "framer-motion", "navigation"],
+      demo: "",
+      code: "https://github.com/RiyasTeche/portfolio-web/tree/main/src",
+
+    },
+  ];
